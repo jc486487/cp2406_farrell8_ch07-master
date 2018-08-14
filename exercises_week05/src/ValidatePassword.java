e30ee521e8fb182f87c5b387e6a9dcc9f93327a6
Sherin24 <sherinsarah.varghese@my.jcu.edu.au>
2018-08-14 16:26:16 +0800

    Exercises of chap 7, week 5

--- exercises_week05/src/ValidatePassword.java
+++ exercises_week05/src/ValidatePassword.java
@@ -5,23 +5,16 @@ public class ValidatePassword
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a password containing atleast 2 uppercase, 2 lowercase and 2 digits: ");
+        System.out.println("Enter a password containing at least 2 uppercase, 2 lowercase and 2 digits: ");
         String password = sc.nextLine();
+        String pattern = "(?=.*[0-9]{2})(?=.*[a-z]{2})(?=.*[A-Z]{2})(?=\\S+$).{6,}";
 
-        int length = password.length();
-
-        while (length < 6)
+        while (!password.matches(pattern))
         {
-            System.out.println("The length should be 6 letters or more; " +
-                    "with at least 2 uppercase, 2 lowercase and 2 digits. \nPlease enter" +
-                    "again: ");
+            System.out.println("The password is incorrect. \nEnter again: ");
             password = sc.nextLine();
         }
 
-        int uppercase = 0;
-        int lowercase = 0;
-        int digits = 0;
-
-
+        System.out.println("The password is correct");
     }
 }
