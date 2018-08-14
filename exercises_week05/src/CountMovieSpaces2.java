52c1fcc88951a78fdc34ef43b87e5f79dcd0a1ca
Sherin24 <sherinsarah.varghese@my.jcu.edu.au>
2018-08-14 11:40:49 +0800

    Exercises of chap 7, week 5

---
+++ exercises_week05/src/CountMovieSpaces2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountMovieSpaces2
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your favorite movie quote: ");
+        String quote = sc.nextLine();
+
+        int length = quote.length();
+        int count = 0;
+
+        for (int i = 0; i <= length; i++)
+        {
+            if (Character.isWhitespace(quote.charAt(i)))
+            {
+                count++;
+            }
+        }
+
+        System.out.println("The total number of spaces in the entered quote is " + count);
+    }
+}
